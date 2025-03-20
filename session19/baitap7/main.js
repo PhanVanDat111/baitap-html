b410b3dad732f2c1a3b7c9b273b37059b06aed63
Phan Van Dat <pdat7737@gmail.com>
2025-03-20 22:28:12 +0700

    sua bai 7

--- session19/baitap7/main.js
+++ session19/baitap7/main.js
@@ -4,7 +4,6 @@ let c = Number(prompt('Nhập số c:'));
 
 let s;
 
-s = (a > b && a > c) ? a + 'là số lớn nhất' : (b > a && b > c) ? b + 'là số lớn nhất' : (c > a && c > b) ? c + 'là số lớn nhất' : "bruhbruh";
-document.writeln(a + ' là số lớn nhất')
-document.writeln(b + ' là số lớn nhất')
-document.writeln(c + ' là số lsớn nhất')  
+s = (a > b && a > c) ? a + ' là số lớn nhất' : (b > a && b > c) ? b + ' là số lớn nhất' : (c > a && c > b) ? c + ' là số lớn nhất' : "bruhbruh";
+
+document.writeln(s)
