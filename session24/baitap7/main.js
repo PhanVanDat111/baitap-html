35074a621dc7682b5baaf5012d1e5776912c2139
Phan Van Dat <pdat7737@gmail.com>
2025-04-01 08:02:23 +0700

    `

--- session24/baitap7/main.js
+++ session24/baitap7/main.js
@@ -1,3 +1,23 @@
 let array = prompt("Mời bạn nhập vào chuỗi").split(" ")
 const upCase = () => alert(array.map(x => x.charAt(0).toUpperCase() + x.slice(1).toLowerCase()).join(" "))
 upCase()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
